20366aefea9a75f451b43f45a7a3aeefb2e47689
richard <binghamrichard123@gmail.com>
2024-02-29 11:47:40 +0000

    Added basic spring boot files

---
+++ TaskManagerJobsAPI/src/main/java/com/grads/ahr/taskmanagerjobsapi/TaskManagerJobsApiApplication.java
@@ -0,0 +1,13 @@
+package com.grads.ahr.taskmanagerjobsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerJobsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerJobsApiApplication.class, args);
+    }
+
+}
---
+++ TaskManagerJobsAPI/src/test/java/com/grads/ahr/taskmanagerjobsapi/TaskManagerJobsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grads.ahr.taskmanagerjobsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerJobsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
